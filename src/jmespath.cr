ad5a4df950d584392ba0254b042c0bd81b4b8440
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2025-02-03 01:48:54 -0300

    add jmespath module search
    Signed-off-by: Alvaro Frias Garay <alvarofriasgaray@gmail.com>

--- src/jmespath.cr
+++ src/jmespath.cr
@@ -1,6 +1,42 @@
-# TODO: Write documentation for `Jmespath`
-module Jmespath
+require "json"
+require "./lexer"
+require "./exceptions"
+require "./ast"
+require "./parser"
+require "./visitor"
+require "./parsed_result"
+
+module JMESPath
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  # Main entry point for JMESPath expressions
+  #
+  # @param expression [String] A valid JMESPath expression
+  # @param data [JSON::Any] The data to search
+  # @param options [Hash] Optional runtime configuration
+  # @return [JSON::Any] Returns the matched values or nil if not found
+  def self.search(expression : String,
+                  data : JSON::Any | Hash(String, JSON::Any) | String | IO,
+                  options = nil) : JSON::Any
+    # Convert input data to JSON::Any if needed
+    json_data = case data
+                when JSON::Any
+                  data
+                when Hash(String, JSON::Any)
+                  JSON::Any.new(data)
+                when String
+                  JSON.parse(data)
+                when IO
+                  JSON.parse(data.gets_to_end)
+                else
+                  raise ArgumentError.new("Invalid data type: #{data.class}")
+                end
+
+    # Create parser and parse expression
+    parser = Parser.new
+    parsed = parser.parse(expression)
+
+    # Search the data using the parsed expression
+    parsed.search(json_data)
+  end
 end
